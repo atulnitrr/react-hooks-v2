22f13dcc51a6b4ff9cbfcf643b79095240bfa3d6
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-15 14:44:32 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Dummy data</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, est? Hic
+        quas exercitationem autem, qui ab quia impedit modi, consectetur, fuga
+        dolore aliquid tempore nemo! Veniam debitis tempora id quo.
+      </p>
+    </>
   );
 }
 
